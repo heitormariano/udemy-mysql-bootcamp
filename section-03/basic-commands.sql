86911f5ead1d4731e326a13db8c1cad9fb96e71e
heitormariano <heitor.eumesmo@gmail.com>
2021-12-21 17:54:12 -0300

    initial commands

---
+++ section-03/basic-commands.sql
@@ -0,0 +1,22 @@
+-- SQL Commands
+
+-- mostrar bases de dados
+show databases;
+
+-- criar uma base de dados
+create database school_app;
+
+-- excluindo uma base de dados
+drop database school_app;
+
+-- usar uma base de dados
+use school_app;
+
+-- identificar o banco de dados usado
+select database();
+
+-- definindo um nome para a coluna do resultado obtido
+select database() as 'Banco Atual';
+
+
+
