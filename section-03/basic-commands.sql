adbc48b57d3088df01a478feb1e4d4f2d1e4e89b
heitormariano <heitor.eumesmo@gmail.com>
2021-12-22 10:18:58 -0300

    new commands: create table, drop table, etc

--- section-03/basic-commands.sql
+++ section-03/basic-commands.sql
@@ -1,22 +1,44 @@
 -- SQL Commands
 
 -- mostrar bases de dados
-show databases;
+SHOW DATABASES;
 
 -- criar uma base de dados
-create database school_app;
+CREATE DATABASE school_app;
 
--- excluindo uma base de dados
-drop database school_app;
+-- excluir uma base de dados
+DROP DATABASE school_app;
 
 -- usar uma base de dados
-use school_app;
+USE school_app;
 
 -- identificar o banco de dados usado
-select database();
+SELECT DATABASE();
 
 -- definindo um nome para a coluna do resultado obtido
-select database() as 'Banco Atual';
+SELECT DATABASE() AS 'Banco Atual';
 
+-- criar banco simples
+CREATE DATABASE testing_db;
+USE testing_db;
 
+-- criar tabela
 
+CREATE TABLE tweets (
+username VARCHAR(15),
+content VARCHAR (145),
+number_favorite INT
+);
+
+-- renomear coluna da tabela
+ALTER TABLE tweets 
+RENAME COLUMN number_favorite TO number_favorites;
+
+-- exibir tabelas do banco
+show tables;
+
+-- ver detalhes de uma tabela específic
+DESC tweets;
+
+-- excluir tabela
+DROP TABLE tweets;
